c61a9cac6b3d9caddd6dae6ac3fb580947b734cf
Tangoes98 <cyriltan985@gmail.com>
2022-11-02 16:30:19 -0400

    Camera Following

--- Assets/_Script/CameraFollowingPlayer.cs
+++ Assets/_Script/CameraFollowingPlayer.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class CameraFollowingPlayer : MonoBehaviour
 {
     public GameObject player;
-    public Camera cam;
-
     private void Update()
     {
+        transform.position = new Vector3(player.transform.position.x + 5f, player.transform.position.y - 1f, -10f);
 
 
     }
